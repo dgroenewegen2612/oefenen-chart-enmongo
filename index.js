433d50133092b8fc3297ffeb7e6d4b5a054f6ff5
dgroenewegen2612 <302782703@student.rocmondriaan.nl>
2024-06-17 17:57:55 +0200

    bodyparser toegevoegd met import statement

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {MongoClient, ServerApiVersion} from 'mongodb';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 
 const app = express();
